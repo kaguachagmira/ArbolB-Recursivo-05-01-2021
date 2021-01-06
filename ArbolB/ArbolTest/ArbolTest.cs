cf9a69311ba6903bd7d72f105672b22373357b45
kaguachagmira <mantillagka@hotmail.com>
2021-01-06 11:18:09 -0500

    Añadido Pruebas unitarias

--- ArbolB/ArbolTest/ArbolTest.cs
+++ ArbolB/ArbolTest/ArbolTest.cs
@@ -1,3 +1,4 @@
+using ArbolB;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace ArbolTest
@@ -6,9 +7,21 @@ namespace ArbolTest
     public class ArbolTest
     {
         [TestMethod]
-        public void nada()
+        public void TestArbolSuma()
         {
+            var arbolOperaciones = new Nodo("+",
+                new Nodo("5"),
+                new Nodo("+",
+                    new Nodo("6"),
+                    new Nodo("+",
+                        new Nodo("-2"),
+                            new Nodo("-1"))));
+            var admin = new Administrador();
+            var resultadoEsperado = 8;
 
+            var resultado = admin.SumarArbol(arbolOperaciones);
+
+            Assert.AreEqual(resultadoEsperado, resultado);
         }
     }
 }
